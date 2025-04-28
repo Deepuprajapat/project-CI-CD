b15de6f9ed01012e555dcb5cc384eb5a13caa447
Deepuprajapat <prajapatideepu003@gmail.com>
2025-04-28 09:37:20 +0000

    modified app.js  index.html file

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 8000;
 
+// Static folder setup (for serving HTML)
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Route for the homepage
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js App deployed via Jenkins CI/CD Pipeline!');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Next page route (for example, to show a different page)
+app.get('/nextpage', (req, res) => {
+    res.send('This is the next page!');
 });
 
 app.listen(PORT, () => {
